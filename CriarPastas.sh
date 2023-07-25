5741628de57332fe6503c3a522ac37d34e0abc71
Lain <ingrid.ruth2@outlook.com>
2023-07-24 21:08:48 -0300

    Atualizacao 01

--- CriarPastas.sh
+++ CriarPastas.sh
@@ -5,17 +5,22 @@
     #pode conter alguns erros de sintaxe em bash pois, foi criado em uma semana
     #de estudos na liguagem.
 
+ 
+ #identificador do diretorio
  diretorio="[seu diretorio]"
- #pasta="Pack"
 
 
+
+ #loop para gerar as pastas
  for ((pastas=1; pastas<=5; pastas++))
  do
      pasta=" Pack $pastas"
     
-
+ #condicao, caso se o diretorio nao exista, sera criado as pastas
 if [ ! -d "$diretorio$pasta" ]; then
 
+       
+       #Criacao das pastas
         mkdir "$diretorio$pasta"
         echo "Pasta criada $pasta"
 
@@ -23,7 +28,8 @@ else
  
         echo "A pasta ja existe, sobrescrevendo ou removendo..."
         rm -rf "$diretorio$pasta"
-
+        #coloquei para remover as pastas, apenas para nao ter pastas 
+        #duplicadas
 
 fi
 done
