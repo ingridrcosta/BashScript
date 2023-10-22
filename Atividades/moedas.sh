1afe94544ebc462ca0a5ed0df4da3d8e14644635
Ingrid Ruth <84602805+ingridrcosta@users.noreply.github.com>
2023-10-22 00:38:33 -0300

    Update moedas.sh
    Definição de classificação apenas, faltou a balança ser adicionada ao código.

--- Atividades/moedas.sh
+++ Atividades/moedas.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+##########################################################################################################################################################
+
+#Versao 01
+
+#Atividade N.º1 do livro - Algorítmos e Lógica da Programação: Um texto Introdutório Para a Engenharia Pag-24 Cap. 1.6
+#Enunciado: 
+#1.1 Descreva como descobrir a moeda falsa em um grupo de cinco moedas
+#fazendo uso de uma balança analítica (sabe-se que a moeda falsa é mais leve que as outras)
+#com o menor número de pesagens possível. Lembre-se de que sua declaração deve resolver
+#o problema para qualquer situação. 
+
+
+#Errata: não tem o modo balança, apenas classsificação, uma nova versão será adicionada com a balança
+##########################################################################################################################################################
 
 moeda01=1
 moeda02=2
